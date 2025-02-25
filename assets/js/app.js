221f883928e4ce9711e438e264b153edd062af4d
khushboo-590 <khushbook4979@gmail.com>
2025-02-25 07:27:50 +0530

    section 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
-// let menuIcon = document.querySelector(".menu-icon");
-// let menuList = document.querySelector(".menu-list");
-// menuIcon.addEventListener("click", () => {
-//     menuList.classList.toggle("show")
-//     let activeNav = menuList.hasAttribute("class", "show");
-//     if (activeNav) { menuIcon.classList.toggle("active") };
-// })
+let menuIcon = document.querySelector(".menu-icon");
+let menuList = document.querySelector(".menu-list");
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("show")
+    let activeNav = menuList.hasAttribute("class", "show");
+    if (activeNav) { menuIcon.classList.toggle("active") };
+})
+
+$(document).ready(function () {
+    $('.my-file').slick({
+        dots: true,
+        infinite: true,
+        speed: 250,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: false,
+        nextArrow: false,
+
+    });
+});
