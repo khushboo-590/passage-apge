b1dd147fe39365affdfbeeb498851a08d931a706
khushboo-590 <khushbook4979@gmail.com>
2025-02-26 09:32:18 +0530

    footer section-mistake slove

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,19 @@ $(document).ready(function () {
 
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
